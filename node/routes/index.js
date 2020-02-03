ec5490ba3b61bca006eff4477db372dc7b7b4120
Walter Traspadini <walter.traspadini@sorint.it>
2020-02-03 18:41:01 +0100

    change in message index.js

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Microsoft learn' });
+  res.render('index', { title: 'Microsoft learn!!' });
 });
 
 module.exports = router;
